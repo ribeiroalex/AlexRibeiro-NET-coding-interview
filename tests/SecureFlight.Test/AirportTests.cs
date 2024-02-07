7d09792ac1511c7104f706b56befa82253d19403
Carlos Varela <carlos.varela@beon.tech>
2024-02-07 12:10:05 -0600

    Adds mock repository

--- tests/SecureFlight.Test/AirportTests.cs
+++ tests/SecureFlight.Test/AirportTests.cs
@@ -1,6 +1,7 @@
-using System;
 using System.Threading.Tasks;
+using Moq;
 using SecureFlight.Core.Entities;
+using SecureFlight.Core.Interfaces;
 using SecureFlight.Infrastructure.Repositories;
 using Xunit;
 
@@ -15,7 +16,7 @@ namespace SecureFlight.Test
             var testingContext = new SecureFlightDatabaseTestContext();
             testingContext.CreateDatabase();
             var repository = new BaseRepository<Airport>(testingContext);
-            
+            var mockRepository = new Mock<IRepository<Airport>>();
             //TODO: Add test code here
             //Act
             
